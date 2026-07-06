59971493468374c97539dae7c6c1c5f13e399a2f
M-VISHAL-KAMATH26 <vishal.kamath@nineleaps.com>
2026-07-06 14:14:31 +0530

    list rendering

--- ReactSQLDumps/latest/src/Components/StudentCard.jsx
+++ ReactSQLDumps/latest/src/Components/StudentCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const StudentCard = ({names="not provided",age="not provided"}) => {
+const StudentCard = ({name="not provided",age="not provided"}) => {
   return (
     <div>
-      <h2>student names:{names}</h2>
+      <h2>student names:{name}</h2>
       <h2>age:{age}</h2>
       <br />
     </div>
--- ReactSQLDumps/latest/src/Components/StudentCardCollection.jsx
+++ ReactSQLDumps/latest/src/Components/StudentCardCollection.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import StudentCard from './StudentCard'
 const StudentCardCollection = () => {
+    const students=[
+        {id:1,name:"Vishal",age:21},
+        {id:2,name:"delson",age:22},
+        {id:3,name:"preya",age:25}
+        ]
   return (
     <div>
-    <StudentCard names='Vishal' age='21'></StudentCard>
+    {/* <StudentCard names='Vishal' age='21'></StudentCard>
         <StudentCard names='delson'></StudentCard>
-    <StudentCard age='21'></StudentCard>
-
+    <StudentCard age='21'></StudentCard> */}
+{
+students.map((student) => (
+          <StudentCard
+            key={student.id}
+            name={student.name}
+            age={student.age}
+          />
+    ))
+}
     </div>
   )
 }
